d642bf7dbc3355aa55f506734432bffa8ecdfd13
01Nijat <mr.nijat.asgarov@gmail.com>
2022-11-23 23:33:24 +0400

    bitdi

---
+++ Delegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+       public delegate bool check(int n);
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(tekeded, 1,2,3,4,5,6));
+            Console.WriteLine(Sum(cuteded, 1, 2, 3, 4, 5, 6));
+        }
+       public static bool tekeded(int n)
+        {
+            return n % 2 != 0;
+        }
+       public static bool cuteded(int n)
+        {
+            return n % 2 == 0;
+        }
+       public static int Sum(check yoxla, params int[] arr)
+        {
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                if (yoxla(item))
+                {
+                    sum += item;
+                }
+                
+            }
+            return sum;
+        }
+    }
+}
